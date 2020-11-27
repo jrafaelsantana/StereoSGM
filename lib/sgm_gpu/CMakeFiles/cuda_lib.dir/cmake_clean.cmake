4c7574d6297aee366dbccea03d0d6d01a57a6d05
Lucas Cambuim <lucascambuim@gmail.com>
2020-11-27 09:37:23 -0300

    removing useless files

--- lib/sgm_gpu/CMakeFiles/cuda_lib.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libcuda_lib.pdb"
-  "libcuda_lib.so"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/cuda_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- lib/sgm_gpu/bin/build-xcode.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-opts=(
-    CUDA_TOOLKIT_ROOT_DIR=/Developer/NVIDIA/CUDA-9.0
-    CUDA_EXAMPLE_USE_CC_EXT=ON
-)
-
-polly.py --toolchain xcode-hid-sections --config Release --fwd ${opts[@]} --verbose ${*}
